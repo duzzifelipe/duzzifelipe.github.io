07738291cfdabea18dcef1d5138463b71d650907
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-06-14 09:45:38 -0300

    feat: index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,45 @@
 import React from "react"
 import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-  </Layout>
-)
+const IndexPage = () => {
+  const posts = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}) {
+        edges {
+          node {
+            frontmatter {
+              slug
+              date(formatString: "DD/MM/YYYY")
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h2>Posts</h2>
+      <div className="post-list">
+      {posts.allMarkdownRemark.edges.map(edge => {
+        const front = edge.node.frontmatter
+
+        return (
+          <div>
+            <Link to={'posts/' + front.slug}>{front.title}</Link>
+            <span>{front.date}</span>
+          </div>
+        )
+      })}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
