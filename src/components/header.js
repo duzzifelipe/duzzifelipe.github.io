588626b05abb264546fa6d4829729947ab7fa918
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-09-09 17:36:22 -0300

    refactor: change styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header style={{ marginBottom: `1.45rem` }}>
     <div
       style={{
         margin: `0 auto`,
@@ -16,17 +11,17 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h2 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
-      </h1>
+      </h2>
     </div>
   </header>
 )
