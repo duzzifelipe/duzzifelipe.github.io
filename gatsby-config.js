fbfb0e1572c19965a26e3e3ef30eb777ccf9db29
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-12-04 19:26:26 -0300

    chore: add plugin for copying gifs

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,12 @@ module.exports = {
             },
           },
           {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {
+              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`]
+            },
+          },
+          {
             resolve: "gatsby-remark-external-links",
             options: {
               target: "_blank",
